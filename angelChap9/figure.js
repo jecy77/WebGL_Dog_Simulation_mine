cb7a5a329a12de628452d96dd41347a49afa337b
MinJ <ussr1285@ajou.ac.kr>
2024-05-23 20:30:18 +0900

    gpt bvh 파일 정보 파싱

--- angelChap9/figure.js
+++ angelChap9/figure.js
@@ -1,33 +1,29 @@
 "use strict";
 
-// 캔버스와 WebGL 컨텍스트, 셰이더 프로그램 초기화
 var canvas;
 var gl;
 var program;
 
-// 투영 행렬과 모델뷰 행렬
 var projectionMatrix;
 var modelViewMatrix;
 
-// 개별 인스턴스에 적용할 행렬
 var instanceMatrix;
 
-// 모델뷰 행렬 위치
 var modelViewMatrixLoc;
 
-// 큐브의 정점 데이터
 var vertices = [
-    vec4(-0.5, -0.5, 0.5, 1.0),
-    vec4(-0.5, 0.5, 0.5, 1.0),
-    vec4(0.5, 0.5, 0.5, 1.0),
-    vec4(0.5, -0.5, 0.5, 1.0),
-    vec4(-0.5, -0.5, -0.5, 1.0),
-    vec4(-0.5, 0.5, -0.5, 1.0),
-    vec4(0.5, 0.5, -0.5, 1.0),
-    vec4(0.5, -0.5, -0.5, 1.0)
+
+    vec4( -0.5, -0.5,  0.5, 1.0 ),
+    vec4( -0.5,  0.5,  0.5, 1.0 ),
+    vec4( 0.5,  0.5,  0.5, 1.0 ),
+    vec4( 0.5, -0.5,  0.5, 1.0 ),
+    vec4( -0.5, -0.5, -0.5, 1.0 ),
+    vec4( -0.5,  0.5, -0.5, 1.0 ),
+    vec4( 0.5,  0.5, -0.5, 1.0 ),
+    vec4( 0.5, -0.5, -0.5, 1.0 )
 ];
 
-// 각 인체 부위 식별자
+
 var torsoId = 0;
 var headId  = 1;
 var head1Id = 1;
@@ -41,7 +37,7 @@ var leftLowerLegId = 7;
 var rightUpperLegId = 8;
 var rightLowerLegId = 9;
 
-// 인체 부위의 크기 설정
+
 var torsoHeight = 5.0;
 var torsoWidth = 1.0;
 var upperArmHeight = 3.0;
@@ -55,12 +51,10 @@ var upperLegHeight = 3.0;
 var headHeight = 1.5;
 var headWidth = 1.0;
 
-// 노드와 각도의 수
 var numNodes = 10;
 var numAngles = 11;
 var angle = 0;
 
-// 초기 각도 설정
 var theta = [0, 0, 0, 0, 0, 0, 180, 0, 180, 0, 0];
 
 var numVertices = 24;
@@ -69,7 +63,7 @@ var stack = [];
 
 var figure = [];
 
-for(var i = 0; i < numNodes; i++) figure[i] = createNode(null, null, null, null);
+for( var i=0; i<numNodes; i++) figure[i] = createNode(null, null, null, null);
 
 var vBuffer;
 var modelViewLoc;
@@ -78,225 +72,318 @@ var pointsArray = [];
 
 //-------------------------------------------
 
-// 스케일링을 위한 함수
 function scale4(a, b, c) {
-    var result = mat4();
-    result[0][0] = a;
-    result[1][1] = b;
-    result[2][2] = c;
-    return result;
+   var result = mat4();
+   result[0][0] = a;
+   result[1][1] = b;
+   result[2][2] = c;
+   return result;
 }
 
 //--------------------------------------------
 
-// 노드 생성 함수
+
 function createNode(transform, render, sibling, child){
     var node = {
-        transform: transform,
-        render: render,
-        sibling: sibling,
-        child: child,
+    transform: transform,
+    render: render,
+    sibling: sibling,
+    child: child,
     }
     return node;
 }
 
-// 노드 초기화 함수
+
 function initNodes(Id) {
+
     var m = mat4();
 
-    // 식별자에 따른 노드 설정
     switch(Id) {
 
-        case torsoId:
-            m = rotate(theta[torsoId], 0, 1, 0);
-            figure[torsoId] = createNode(m, torso, null, headId);
-            break;
+    case torsoId:
 
-        case headId:
-        case head1Id:
-        case head2Id:
-            m = translate(0.0, torsoHeight + 0.5 * headHeight, 0.0);
-            m = mult(m, rotate(theta[head1Id], 1, 0, 0))
-            m = mult(m, rotate(theta[head2Id], 0, 1, 0));
-            m = mult(m, translate(0.0, -0.5 * headHeight, 0.0));
-            figure[headId] = createNode(m, head, leftUpperArmId, null);
-            break;
+    m = rotate(theta[torsoId], 0, 1, 0 );
+    figure[torsoId] = createNode( m, torso, null, headId );
+    break;
 
-        case leftUpperArmId:
-            m = translate(-(torsoWidth + upperArmWidth), 0.9 * torsoHeight, 0.0);
-            m = mult(m, rotate(theta[leftUpperArmId], 1, 0, 0));
-            figure[leftUpperArmId] = createNode(m, leftUpperArm, rightUpperArmId, leftLowerArmId);
-            break;
+    case headId:
+    case head1Id:
+    case head2Id:
 
-        case rightUpperArmId:
-            m = translate(torsoWidth + upperArmWidth, 0.9 * torsoHeight, 0.0);
-            m = mult(m, rotate(theta[rightUpperArmId], 1, 0, 0));
-            figure[rightUpperArmId] = createNode(m, rightUpperArm, leftUpperLegId, rightLowerArmId);
-            break;
 
-        case leftUpperLegId:
-            m = translate(-(torsoWidth + upperLegWidth), 0.1 * upperLegHeight, 0.0);
-            m = mult(m, rotate(theta[leftUpperLegId], 1, 0, 0));
-            figure[leftUpperLegId] = createNode(m, leftUpperLeg, rightUpperLegId, leftLowerLegId);
-            break;
+    m = translate(0.0, torsoHeight+0.5*headHeight, 0.0);
+	m = mult(m, rotate(theta[head1Id], 1, 0, 0))
+	m = mult(m, rotate(theta[head2Id], 0, 1, 0));
+    m = mult(m, translate(0.0, -0.5*headHeight, 0.0));
+    figure[headId] = createNode( m, head, leftUpperArmId, null);
+    break;
 
-        case rightUpperLegId:
-            m = translate(torsoWidth + upperLegWidth, 0.1 * upperLegHeight, 0.0);
-            m = mult(m, rotate(theta[rightUpperLegId], 1, 0, 0));
-            figure[rightUpperLegId] = createNode(m, rightUpperLeg, null, rightLowerLegId);
-            break;
 
-        case leftLowerArmId:
-            m = translate(0.0, upperArmHeight, 0.0);
-            m = mult(m, rotate(theta[leftLowerArmId], 1, 0, 0));
-            figure[leftLowerArmId] = createNode(m, leftLowerArm, null, null);
-            break;
+    case leftUpperArmId:
 
-        case rightLowerArmId:
-            m = translate(0.0, upperArmHeight, 0.0);
-            m = mult(m, rotate(theta[rightLowerArmId], 1, 0, 0));
-            figure[rightLowerArmId] = createNode(m, rightLowerArm, null, null);
-            break;
+    m = translate(-(torsoWidth+upperArmWidth), 0.9*torsoHeight, 0.0);
+	m = mult(m, rotate(theta[leftUpperArmId], 1, 0, 0));
+    figure[leftUpperArmId] = createNode( m, leftUpperArm, rightUpperArmId, leftLowerArmId );
+    break;
 
-        case leftLowerLegId:
-            m = translate(0.0, upperLegHeight, 0.0);
-            m = mult(m, rotate(theta[leftLowerLegId], 1, 0, 0));
-            figure[leftLowerLegId] = createNode(m, leftLowerLeg, null, null);
-            break;
+    case rightUpperArmId:
 
-        case rightLowerLegId:
-            m = translate(0.0, upperLegHeight, 0.0);
-            m = mult(m, rotate(theta[rightLowerLegId], 1, 0, 0));
-            figure[rightLowerLegId] = createNode(m, rightLowerLeg, null, null);
-            break;
+    m = translate(torsoWidth+upperArmWidth, 0.9*torsoHeight, 0.0);
+	m = mult(m, rotate(theta[rightUpperArmId], 1, 0, 0));
+    figure[rightUpperArmId] = createNode( m, rightUpperArm, leftUpperLegId, rightLowerArmId );
+    break;
+
+    case leftUpperLegId:
+
+    m = translate(-(torsoWidth+upperLegWidth), 0.1*upperLegHeight, 0.0);
+	m = mult(m , rotate(theta[leftUpperLegId], 1, 0, 0));
+    figure[leftUpperLegId] = createNode( m, leftUpperLeg, rightUpperLegId, leftLowerLegId );
+    break;
+
+    case rightUpperLegId:
+
+    m = translate(torsoWidth+upperLegWidth, 0.1*upperLegHeight, 0.0);
+	m = mult(m, rotate(theta[rightUpperLegId], 1, 0, 0));
+    figure[rightUpperLegId] = createNode( m, rightUpperLeg, null, rightLowerLegId );
+    break;
+
+    case leftLowerArmId:
+
+    m = translate(0.0, upperArmHeight, 0.0);
+    m = mult(m, rotate(theta[leftLowerArmId], 1, 0, 0));
+    figure[leftLowerArmId] = createNode( m, leftLowerArm, null, null );
+    break;
+
+    case rightLowerArmId:
+
+    m = translate(0.0, upperArmHeight, 0.0);
+    m = mult(m, rotate(theta[rightLowerArmId], 1, 0, 0));
+    figure[rightLowerArmId] = createNode( m, rightLowerArm, null, null );
+    break;
+
+    case leftLowerLegId:
+
+    m = translate(0.0, upperLegHeight, 0.0);
+    m = mult(m, rotate(theta[leftLowerLegId], 1, 0, 0));
+    figure[leftLowerLegId] = createNode( m, leftLowerLeg, null, null );
+    break;
+
+    case rightLowerLegId:
+
+    m = translate(0.0, upperLegHeight, 0.0);
+    m = mult(m, rotate(theta[rightLowerLegId], 1, 0, 0));
+    figure[rightLowerLegId] = createNode( m, rightLowerLeg, null, null );
+    break;
+
     }
+
 }
 
-// 트리 구조를 순회하며 각 노드를 그리는 함수
 function traverse(Id) {
-    if(Id == null) return;
-    stack.push(modelViewMatrix);
-    modelViewMatrix = mult(modelViewMatrix, figure[Id].transform);
-    figure[Id].render();
-    if(figure[Id].child != null) traverse(figure[Id].child);
+
+   if(Id == null) return;
+   stack.push(modelViewMatrix);
+   modelViewMatrix = mult(modelViewMatrix, figure[Id].transform);
+   figure[Id].render();
+   if(figure[Id].child != null) traverse(figure[Id].child);
     modelViewMatrix = stack.pop();
-    if(figure[Id].sibling != null) traverse(figure[Id].sibling);
+   if(figure[Id].sibling != null) traverse(figure[Id].sibling);
 }
 
-// 각 부위를 그리는 함수들
 function torso() {
-    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * torsoHeight, 0.0));
-    instanceMatrix = mult(instanceMatrix, scale4(torsoWidth, torsoHeight, torsoWidth));
+
+    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5*torsoHeight, 0.0) );
+    instanceMatrix = mult(instanceMatrix, scale4( torsoWidth, torsoHeight, torsoWidth));
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
-    for(var i = 0; i < 6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4 * i, 4);
+    for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
 }
 
 function head() {
-    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * headHeight, 0.0));
-    instanceMatrix = mult(instanceMatrix, scale4(headWidth, headHeight, headWidth));
+
+    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * headHeight, 0.0 ));
+	instanceMatrix = mult(instanceMatrix, scale4(headWidth, headHeight, headWidth) );
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
-    for(var i = 0; i < 6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4 * i, 4);
+    for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
 }
 
-// 큐브를 구성하는 함수
-function cube() {
-    quad(1, 0, 3, 2);
-    quad(2, 3, 7, 6);
-    quad(3, 0, 4, 7);
-    quad(6, 5, 1, 2);
-    quad(4, 5, 6, 7);
-    quad(5, 4, 0, 1);
+function leftUpperArm() {
+
+    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * upperArmHeight, 0.0) );
+	instanceMatrix = mult(instanceMatrix, scale4(upperArmWidth, upperArmHeight, upperArmWidth) );
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
+    for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
 }
 
-// 페이지 로드 시 초기화 및 렌더링 설정
+function leftLowerArm() {
+
+    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * lowerArmHeight, 0.0) );
+	instanceMatrix = mult(instanceMatrix, scale4(lowerArmWidth, lowerArmHeight, lowerArmWidth) );
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
+    for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
+}
+
+function rightUpperArm() {
+
+    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * upperArmHeight, 0.0) );
+	instanceMatrix = mult(instanceMatrix, scale4(upperArmWidth, upperArmHeight, upperArmWidth) );
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
+    for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
+}
+
+function rightLowerArm() {
+
+    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * lowerArmHeight, 0.0) );
+	instanceMatrix = mult(instanceMatrix, scale4(lowerArmWidth, lowerArmHeight, lowerArmWidth) );
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
+    for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
+}
+
+function  leftUpperLeg() {
+
+    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * upperLegHeight, 0.0) );
+	instanceMatrix = mult(instanceMatrix, scale4(upperLegWidth, upperLegHeight, upperLegWidth) );
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
+    for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
+}
+
+function leftLowerLeg() {
+
+    instanceMatrix = mult(modelViewMatrix, translate( 0.0, 0.5 * lowerLegHeight, 0.0) );
+	instanceMatrix = mult(instanceMatrix, scale4(lowerLegWidth, lowerLegHeight, lowerLegWidth) );
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
+    for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
+}
+
+function rightUpperLeg() {
+
+    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * upperLegHeight, 0.0) );
+	instanceMatrix = mult(instanceMatrix, scale4(upperLegWidth, upperLegHeight, upperLegWidth) );
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
+    for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
+}
+
+function rightLowerLeg() {
+
+    instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * lowerLegHeight, 0.0) );
+	instanceMatrix = mult(instanceMatrix, scale4(lowerLegWidth, lowerLegHeight, lowerLegWidth) )
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
+    for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
+}
+
+function quad(a, b, c, d) {
+     pointsArray.push(vertices[a]);
+     pointsArray.push(vertices[b]);
+     pointsArray.push(vertices[c]);
+     pointsArray.push(vertices[d]);
+}
+
+
+function cube()
+{
+    quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    quad( 5, 4, 0, 1 );
+}
+
+
 window.onload = function init() {
-    canvas = document.getElementById("gl-canvas");
-    gl = WebGLUtils.setupWebGL(canvas);
-    if (!gl) {
-        alert("WebGL isn't available");
-        return;
-    }
 
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    canvas = document.getElementById( "gl-canvas" );
 
-    program = initShaders(gl, "vertex-shader", "fragment-shader");
-    gl.useProgram(program);
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    program = initShaders( gl, "vertex-shader", "fragment-shader");
+
+    gl.useProgram( program);
 
     instanceMatrix = mat4();
-    projectionMatrix = ortho(-10.0, 10.0, -10.0, 10.0, -10.0, 10.0);
+
+    projectionMatrix = ortho(-10.0,10.0,-10.0, 10.0,-10.0,10.0);
     modelViewMatrix = mat4();
 
-    gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelViewMatrix"), false, flatten(modelViewMatrix));
-    gl.uniformMatrix4fv(gl.getUniformLocation(program, "projectionMatrix"), false, flatten(projectionMatrix));
 
-    modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
+    gl.uniformMatrix4fv(gl.getUniformLocation( program, "modelViewMatrix"), false, flatten(modelViewMatrix) );
+    gl.uniformMatrix4fv( gl.getUniformLocation( program, "projectionMatrix"), false, flatten(projectionMatrix) );
+
+    modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix")
 
     cube();
 
     vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
     gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
 
-    var vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
 
-    // 슬라이더 이벤트 핸들러 연결
-    document.getElementById("slider0").onchange = function(event) {
-        theta[torsoId] = event.target.value;
+        document.getElementById("slider0").onchange = function(event) {
+        theta[torsoId ] = event.target.value;
         initNodes(torsoId);
     };
-    document.getElementById("slider1").onchange = function(event) {
+        document.getElementById("slider1").onchange = function(event) {
         theta[head1Id] = event.target.value;
         initNodes(head1Id);
     };
+
     document.getElementById("slider2").onchange = function(event) {
-        theta[leftUpperArmId] = event.target.value;
-        initNodes(leftUpperArmId);
+         theta[leftUpperArmId] = event.target.value;
+         initNodes(leftUpperArmId);
     };
     document.getElementById("slider3").onchange = function(event) {
-        theta[leftLowerArmId] = event.target.value;
-        initNodes(leftLowerArmId);
+         theta[leftLowerArmId] =  event.target.value;
+         initNodes(leftLowerArmId);
     };
-    document.getElementById("slider4").onchange = function(event) {
+
+        document.getElementById("slider4").onchange = function(event) {
         theta[rightUpperArmId] = event.target.value;
         initNodes(rightUpperArmId);
     };
     document.getElementById("slider5").onchange = function(event) {
-        theta[rightLowerArmId] = event.target.value;
-        initNodes(rightLowerArmId);
+         theta[rightLowerArmId] =  event.target.value;
+         initNodes(rightLowerArmId);
     };
-    document.getElementById("slider6").onchange = function(event) {
+        document.getElementById("slider6").onchange = function(event) {
         theta[leftUpperLegId] = event.target.value;
         initNodes(leftUpperLegId);
     };
     document.getElementById("slider7").onchange = function(event) {
-        theta[leftLowerLegId] = event.target.value;
-        initNodes(leftLowerLegId);
+         theta[leftLowerLegId] = event.target.value;
+         initNodes(leftLowerLegId);
     };
     document.getElementById("slider8").onchange = function(event) {
-        theta[rightUpperLegId] = event.target.value;
-        initNodes(rightUpperLegId);
+         theta[rightUpperLegId] =  event.target.value;
+         initNodes(rightUpperLegId);
     };
-    document.getElementById("slider9").onchange = function(event) {
+        document.getElementById("slider9").onchange = function(event) {
         theta[rightLowerLegId] = event.target.value;
         initNodes(rightLowerLegId);
     };
     document.getElementById("slider10").onchange = function(event) {
-        theta[head2Id] = event.target.value;
-        initNodes(head2Id);
+         theta[head2Id] = event.target.value;
+         initNodes(head2Id);
     };
 
-    // 모든 노드 초기화
-    for(var i = 0; i < numNodes; i++) initNodes(i);
+    for(i=0; i<numNodes; i++) initNodes(i);
 
     render();
 }
 
-// 렌더링 함수
+
 var render = function() {
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    traverse(torsoId);
-    requestAnimFrame(render);
+
+        gl.clear( gl.COLOR_BUFFER_BIT );
+        traverse(torsoId);
+        requestAnimFrame(render);
 }
